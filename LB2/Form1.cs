bbb9f99ed1eedbd583bcd1a544b30f541a1dbc83
graschenkov-beis19 <deadrater.sbmpei@gmail.com>
2021-03-19 09:16:25 +0300

    Lab-1

---
+++ LB2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LB2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            x = x + y;
+            textBox3.Text = Convert.ToString(x);
+            textBox4.Text = ("Сложение");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            x = x - y;
+            textBox3.Text = Convert.ToString(x);
+            textBox4.Text = ("Вычитание");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            x = x * y;
+            textBox3.Text = Convert.ToString(x);
+            textBox4.Text = ("Умножение");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            x = x / y;
+            textBox3.Text = Convert.ToString(x);
+            textBox4.Text = ("Деление");
+        }
+    }
+}
